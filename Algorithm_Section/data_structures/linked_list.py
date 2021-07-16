f440fa4ecdf31701b2e142884437a9c1db7edd82
Teghfo <teghfo@gmail.com>
2021-07-16 19:23:22 +0430

    complete linkedlist

--- Algorithm_Section/data_structures/linked_list.py
+++ Algorithm_Section/data_structures/linked_list.py
@@ -17,6 +17,9 @@ class LinkedListNode:
     def node_busy(self):
         self._has_refrence = True
 
+    def __str__(self):
+        return str(self.data)
+
 
 class LinkedList:
     def __init__(self):
@@ -32,14 +35,61 @@ class LinkedList:
             self.head = node
         node.node_busy()
 
+    # def __next__(self):
+    #     pass
+
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp
+            temp = temp.next
+
     def append(self, node: LinkedListNode):
-        pass
+        assert node.has_refrence == False, "say kon tak par bashi! inja sarzamin monogamus hast."
+
+        if not self.head:
+            self.head = node
+        else:
+            temp = self.head
+            # while temp:
+            #     if not temp.next:
+            #         temp.next = node
+            #         break
+            #     temp = temp.next
+            while temp.next:
+                temp = temp.next
+            temp.next = node
+        node.node_busy()
 
     def pop(self, index=-1):
         pass
 
     def insert(self, node: LinkedListNode, index: int):
-        pass
+        assert node.has_refrence == False, "say kon tak par bashi! inja sarzamin monogamus hast."
+
+        if not self.head:
+            self.head = node
+
+        if index == 0:
+            self.add_begin(node)
+        elif index > len(self)-1:
+            self.append(node)
+        elif index < 0:
+            index += len(self)
+            if index < 0:
+                raise IndexError("index out of range")
+            self.insert(node, index)
+        else:
+            count = 0
+            temp = self.head
+            while temp.next:
+                if count == index - 1:
+                    node.next = temp.next
+                    temp.next = node
+                    break
+                temp = temp.next
+                count += 1
+        node.node_busy()
 
     def remove(self, node: LinkedListNode):
         pass
@@ -78,8 +128,12 @@ node4 = LinkedListNode(25)
 
 linkedlist1.add_begin(node1)
 linkedlist1.add_begin(node2)
-linkedlist1.add_begin(node3)
-linkedlist1.add_begin(node4)
+linkedlist1.append(node4)
+linkedlist1.insert(node3, 2)
 
 print(linkedlist1)
 print(len(linkedlist1))
+
+
+# a = list(linkedlist1)
+# print(a[0])
